449a5c7a19e6e0d69d3d04f79755bf2613ec8909
Juan Ignacio Lopez <juan.ignacio848@gmail.com>
2025-02-03 18:27:49 -0300

    Change cursor depending on vi mode

--- fish/conf.d/03_prompt.fish
+++ fish/conf.d/03_prompt.fish
@@ -1,1 +1,13 @@
 starship init fish | source
+
+function fish_mode_prompt
+    if test "$fish_bind_mode" = "insert"
+        # Change cursor to a vertical bar in insert mode
+        echo -ne "\e[5 q"  # Escape sequence for I-beam (|) cursor
+    else
+        # Change cursor back to block in normal mode
+        echo -ne "\e[2 q"  # Escape sequence for block cursor
+    end
+end
+
+fish_mode_prompt
